7c1005e345480c1648de3a0b012e3278467b384b
marcthe12 <marcboocha@gmail.com>
2016-05-07 13:40:20 +0800

    made simple notepad
    Signed-off-by: marcthe12 <marcboocha@gmail.com>

--- colour/src/colour.c
+++ colour/src/colour.c
@@ -3,14 +3,43 @@
 int main ()
 {
 	initscr();
-	no_echo();
-	cbreak();
-	key_pad(stdscr,True);
+	noecho();
+	raw();
+	keypad(stdscr, TRUE);
+	int x,y;
 	int key=getch();
-	while(!key==27){
-		switch(key)
-			case(KEY_UP)
-			case(KEY_DOWN)
+	while(key != KEY_F(10)){
+		switch(key){
+			case(KEY_UP):
+				getyx(stdscr,y,x);
+				y--;
+				move(y,x);
+				refresh();
+				break;
+			case(KEY_DOWN):
+				getyx(stdscr,y,x);
+				y++;
+				move(y,x);
+				refresh();
+				break;
+			case(KEY_LEFT):
+				getyx(stdscr,y,x);
+				x--;
+				move(y,x);
+				refresh();
+				break;
+			case(KEY_RIGHT):
+				getyx(stdscr,y,x);
+				x++;
+				move(y,x);
+				refresh();
+				break;
+			default:
+				printw("%c",key);
+				refresh();
+				break;
+		};
+	key=getch();
 	};
 	endwin();
 	return (0);
